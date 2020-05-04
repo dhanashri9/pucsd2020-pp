335e5a53c49d76c32b1d4ca790af009dd8bde028
dhanashri9 <33326652+dhanashri9@users.noreply.github.com>
2020-05-04 20:24:50 +0530

    Update 001_create_table.sql

--- resource/sql/001_create_table.sql
+++ resource/sql/001_create_table.sql
@@ -1,14 +1,59 @@
-USE restapi;
+CREATE TABLE IF NOT EXISTS RoleType(
+	role_id 	INT		PRIMARY KEY,
+	role 		VARCHAR(20)		UNIQUE KEY
+);
 
-CREATE TABLE IF NOT EXISTS user_detail (
-    id                  INT         AUTO_INCREMENT      PRIMARY KEY,
-    first_name          CHAR(25)    NOT NULL,
-    last_name           CHAR(25)    NOT NULL,
-    email               CHAR(64)    NOT NULL UNIQUE,
-    password            VARBINARY(128)    NOT NULL,
-    contact_number      CHAR(15)    NOT NULL,
-    updated_by          INT         NOT NULL DEFAULT 0,
-    deleted             TINYINT(1)  NOT NULL DEFAULT 0,
-    creation_date       DATETIME    DEFAULT CURRENT_TIMESTAMP,
-    last_update         DATETIME    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)ENGINE = INNODB CHARACTER SET=utf8;
+
+CREATE TABLE IF NOT EXISTS user (
+	user_id 	INT		AUTO_INCREMENT 		PRIMARY KEY,
+	first_name	CHAR(25) 	NOT NULL,
+	last_name	CHAR(25) 	NOT NULL,
+	password 	VARBINARY(128)	NOT NULL,
+	creation_date	DATETIME	DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE IF NOT EXISTS group (
+	group_id 	INT 	AUTO_INCREMENT PRIMARY KEY,
+	group_name	CHAR(25),
+	group_info	CHAR(50),
+	group_creation_date		DATETIME 	DEFAULT CURRENT_TIMESTAMP 
+);
+
+
+CREATE TABLE IF NOT EXISTS userGroup (
+	user_id 	INT,
+	group_id 	INT,
+	PRIMARY KEY (user_id, group_id),
+	FOREIGN KEY (user_id) REFERENCES user(user_id),
+	FOREIGN KEY (group_id) REFERENCES group(group_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS file (
+	file_id		INT 	AUTO_INCREMENT 	PRIMARY KEY,
+	file_name	CHAR(20),
+	file_type 	CHAR(20),
+	parent_id 	INT,
+	FOREIGN KEY (parent_id) REFERENCES file(file_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS userFilePermission (
+	user_id 	INT,
+	file_id 	INT,
+	role_id 	INT,
+	FOREIGN KEY (user_id) REFERENCES user(user_id),
+	FOREIGN KEY (file_id) REFERENCES file(file_id),
+	FOREIGN KEY (role_id) REFERENCES RoleType(role_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS groupFilePermission (
+	group_id 	INT,
+	file_id 	INT,
+	role_id 	INT,
+	FOREIGN KEY (group_id) REFERENCES group(group_id),
+	FOREIGN KEY (file_id) REFERENCES file(file_id),
+	FOREIGN KEY (role_id) REFERENCES RoleType(role_id)
+);
